308b8ef30be83793b8072f3bdecd40efed3343af
patricio <lobochile275@gmail.com>
2013-10-14 18:29:00 -0300

    Posts and Welcome controllers created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Blog::Application.routes.draw do
+  #get "welcome/index"
+
+  root "welcome#index"
+
+  resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
