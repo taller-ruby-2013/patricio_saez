ac8ac227adb9a0ff2b74af774d18fa88d3867ff9
patricio <lobochile275@gmail.com>
2013-11-04 18:53:30 -0300

    ActiveRecord functionality integrated; Post table created; All controllers and views updated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,25 @@ class PostsController < ApplicationController
 	def new
 	end
 
-	def create
-		render text: params[:post].inspect
+	def index
+		@posts = Post.all
 	end
+
+	def create
+	  @post = Post.new(params[:post])
+	 
+	  @post.save
+	  redirect_to @post
+	end
+
+	def show
+	  @post = Post.find(params[:id])
+	end
+	
+=begin
+	private
+	  def post_params
+	    params.require(:post).permit(:title, :text)
+	  end
+=end
 end
