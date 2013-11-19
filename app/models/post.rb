5f20161eae4dd8eb095fadd1c8810a926fc425fd
patricio <lobochile275@gmail.com>
2013-11-19 04:22:33 -0300

    [Sixth class] Refactoring code (new comments view added)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :text, :title
 
-  has_many :comments
+  has_many :comments, dependent: :destroy	
 
   validates :title, :presence =>  true, :length => { :minimum => 5 }
 end
